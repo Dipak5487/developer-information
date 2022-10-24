5d4f80d1eff7c92a57b2ce82faf1278f1e1abef8
Dipak5487 <dipakchauhan330@gmail.com>
2022-10-24 23:32:31 +0530

    Log in pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { UserLogInComponent } from './user-log-in/user-log-in.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl,FormGroup,Validators,FormBuilder } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLogInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-log-in/user-log-in.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup,Validators,FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-log-in',
+  templateUrl: './user-log-in.component.html',
+  styleUrls: ['./user-log-in.component.css']
+})
+export class UserLogInComponent implements OnInit {
+
+    //Form Validables 
+registerForm:any = FormGroup;
+submitted = false;
+constructor( private formBuilder: FormBuilder){}
+//Add user form actions
+get f() { return this.registerForm.controls; }
+onSubmit() {
+  
+  this.submitted = true;
+  // stop here if form is invalid
+  if (this.registerForm.invalid) {
+      return;
+  }
+  //True if all the fields are filled
+  if(this.submitted)
+  {
+    console.log("Email is",this.registerForm.value.email)
+    console.log("Email is",this.registerForm.value.password)
+    alert("Great!!");
+  }
+ 
+}
+  ngOnInit() {
+    //Add User form validations
+    this.registerForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+    });
+  }
+
+}
